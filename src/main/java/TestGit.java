786128aa960afd98e75d157ddb2e1c8a307d3e8c
summer <1397492499@qq.com>
2022-07-24 21:15:46 +0800

    idea集成git第4次提交,测试分支冲突合并

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -1,8 +1,9 @@
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("hello git!!");
+        System.out.println("hello git!!, conflict1");
         System.out.println("测试idea集成git：第二个版本");
         System.out.println("the third version");
+        System.out.println("master conflict merge");
     }
 
 
