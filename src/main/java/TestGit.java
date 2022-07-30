ce33a0d59a7e9973c8d3e3cad8326285b367bc78
summer <1397492499@qq.com>
2022-07-30 14:39:52 +0800

    idea集成github，测试push功能

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -5,8 +5,10 @@ public class TestGit {
         System.out.println("the third version");
         System.out.println("hot-fix conflict merge");
         System.out.println("master conflict merge");
+        System.out.println("idea集成GitHub测试push功能");
     }
-
+//GitHub口令
+//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
 
 
 }
