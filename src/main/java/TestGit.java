243046d60bccc3216f43aae9fc5d3fd1dcb1a7ee
SummerLeoo <71005172+SummerLeoo@users.noreply.github.com>
2022-07-30 15:36:23 +0800

    idea集成github测试pull功能

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -6,6 +6,7 @@ public class TestGit {
         System.out.println("hot-fix conflict merge");
         System.out.println("master conflict merge");
         System.out.println("idea集成GitHub测试push功能");
+        System.out.println("idea集成github测试pull功能");
     }
 //GitHub口令
 //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
